88d4f7826d34b69e75fb61b23bd08c0aea99caea
mati <maticolis1999_sacchi@hotmail.com>
2022-07-31 18:26:30 -0300

    Creacion de componentes y buldeado de app angular

--- Grupetto_Angular/Grupetto/Grupetto/src/app/app.module.ts
+++ Grupetto_Angular/Grupetto/Grupetto/src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainpComponent } from './components/mainp/mainp.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    MainpComponent,
+    RegisterComponent,
+
   ],
   imports: [
     BrowserModule,
